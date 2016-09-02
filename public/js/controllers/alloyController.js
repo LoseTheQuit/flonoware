2f79c37b634c46cd67d0001792f953dfeaf01c7e
Brendan Carr <losethequit@gmail.com>
2016-09-02 15:31:16 -0400

    flono - 008

--- public/js/controllers/alloyController.js
+++ public/js/controllers/alloyController.js
@@ -156,7 +156,7 @@ app.controller('alloyController', function ($scope, $http, alloyService) {
             // console.info('Lat: ' + $scope.randomLat + ' Long: ' + $scope.randomLong);
 
             $scope.collectionPlate();
-            if ($scope.totalAmountOfItemsInThisColletection <= 1000) {
+            if ($scope.totalAmountOfItemsInThisColletection <= 10) {
                 $scope.customAdd();
             } else {
                 $scope.deleteAll();
