56ff2150f7d815cef8ce094775c4d3f7e4b03171
Brendan Carr <losethequit@gmail.com>
2016-11-15 08:28:56 -0500

    switching editors

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,93 @@
 'use strict';
 
 var gulp = require('gulp');
+var del = require('del');
+var pump = require('pump');
 var clean = require('gulp-clean');
-var babel = require('gulp-babel');
 var rename = require('gulp-rename');
 var gutil = require('gulp-util');
 var sass = require('gulp-sass');
-var minify = require('gulp-minify');
-var jshint = require('gulp-jshint');
+var cssmin = require('gulp-cssmin');
 var concat = require('gulp-concat');
+var minify = require('gulp-minify');
 var uglify = require('gulp-uglify');
+var maps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
+var jshint = require('gulp-jshint');
+var babel = require('gulp-babel');
 
-
-gulp.task('clean', function() {
-    return gulp.src('public/dist', {read: false}).pipe(clean());
+gulp.task('del', function() {
+    del(['dist']);
 });
 
-// // Delete the dist directory
-// gulp.task('clean', function() {
-//     return gulp.src(bases.dist).pipe(clean());
-// });
-
-
-// A development task to run anytime a file changes
-gulp.task('watch', function() {
-    console.log('GULP WATCH');
-    gulp.watch('public/**/*', ['default']);
+gulp.task('clean', ['del'], function() {
+    return gulp.src('public/dist', { read: false }).pipe(clean())
 });
 
 gulp.task('concatAngular', function() {
     gulp.src([
-      'public/js/ng/controllers/*.js',
-      'public/js/ng/*services/*.js',
-      'public/js/ng/directives/*.js'
-    ])
-    .pipe(concat('init.js'))
-    .pipe(gulp.dest('public/dist/js/ng'));
+            'public/js/ng/controllers/*.js',
+            'public/js/ng/services/*.js',
+            'public/js/ng/directives/*.js'
+        ])
+        // .pipe(maps.init())
+        .pipe(concat('ng.js'))
+        // .pipe(maps.write('./'))
+        // .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('public/dist/js'));
 });
 
 gulp.task('uglifyAngular', ['concatAngular'], function() {
-    gulp.src('public/dist/js/ng/init.js')
-    .pipe(uglify())
-    .pipe(rename('init.min.js'))
-    .pipe(babel({
-             presets: ['es2015'], //this fixed a shorthand javascript error
-             compact: true
-         }))
-    .pipe(gulp.dest('public/dist/js/ng/dist'));
+    gulp.src('public/dist/js/*.js')
+        .pipe(uglify())
+        .pipe(rename('ng.min.js'))
+        .pipe(babel({
+            presets: ['es2015'], //this fixed a shorthand javascript error
+            compact: true
+        }))
+        .pipe(gulp.dest('public/dist/js/dist'));
 });
 
-gulp.task('compileSass', function() {
-    gulp.src('public/scss/index.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('public/dist/sass/'));
+gulp.task('compress', ['concatAngular'], function(cb) {
+    pump([
+            gulp.src('public/dist/js/*.js'),
+            uglify(),
+            gulp.dest('dist')
+        ],
+        cb
+    );
 });
 
-// Define the default task as a sequence of the above tasks
-// gulp.task('default', ['clean', 'scripts', 'copy']);
-// gulp.task('default', ['clean', 'scripts', 'imagemin', 'copy']);
+gulp.task('compileMinifySass', function() {
+    gulp.src('public/sass/**/*.scss')
+        .pipe(maps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        // .pipe(gulp.dest('dist'))
+        .pipe(maps.write('../maps'))
+        .pipe(gulp.dest('public/dist/css'))
+});
 
-gulp.task('default', ['clean'], function() {
-    // gulp.start('paths');
-    gulp.start('uglifyAngular');
-    gulp.start('compileSass');
-    // gulp.start('scripts');
-    // gulp.start('imagemin');
-    // gulp.start('copy');
-    // gulp.start('sass');
-    // gulp.start('watch');
+gulp.task('watch', function() {
+    console.log('START: GULP WATCH');
+    // gulp.watch('public/sass/**/*', ['compileMinifySass']);
+    // gulp.watch('public/js/ng/**/*', ['compress']);
+    gulp.watch('public/**/*', ['default']);
 
-    // gulp.start('compress');
-    // gulp.start('watch');
+});
+gulp.task('serve', ['watch']);
+
+gulp.task('build', ['clean'], function() {
+    gulp.start('compress');
+    gulp.start('compileMinifySass');
+
+    // ,'public/img/**/*'
+    // ,'public/dist/js/**/*', 'public/dist/js/ng.js'
+});
+
+gulp.task('default', ['build'], function() {
+    return gulp.src(['public/views/**/*'], { base: './' })
+        .pipe(gulp.dest('dist'));
+    // gulp.start('build')
 });
